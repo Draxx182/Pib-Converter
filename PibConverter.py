b24cb7f19a194bc3483b8e55f51d0ec41cb5c91e
Draxx182 <82241042+Draxx182@users.noreply.github.com>
2022-03-03 21:14:15 -0600

    Update PibConverter.py

--- PibConverter.py
+++ PibConverter.py
@@ -35,63 +35,76 @@ def BinaryCombine(read, write, type): #Takes the Reader Buffer and the Writer Bu
         write.write_bytes(output)
     return output #Returns what the Reader had intially sent
 
+def IshinWrite(ishin_path):
+    wr = BinaryReader()
+    wr.set_endian(True)
+    #//Read//
+    with open(ishin_path, "rb") as f:
+        rd = BinaryReader(f.read())
+        rd.set_endian(True)
+
+        #--File Header--
+        fileInfo = BinaryCombine(rd, wr, 0x8)
+        fileVersion = rd.read_uint32()
+        if (fileVersion == 25):
+            wr.write_uint32(27)
+        else:
+            return str(ishin_path)+" not converted! (Pib not correct version)"
+        fileFunny = BinaryCombine(rd, wr, 4)
+        #--File Information--
+        pibID = BinaryCombine(rd, wr, 4)
+        numOfDDS = BinaryCombine(rd, wr, 4)
+        BinaryCombine(rd, wr, 0x2A8)
+
+        for DDS in range(numOfDDS):
+            #--DDS Info--
+            BinaryCombine(rd, wr, 0x8)
+            if (fileVersion == 25): #--Retraso: add group at 0x4 
+                wr.write_uint32(0)
+            BinaryCombine(rd, wr, 0x78)
+            if (fileVersion == 25): #Credit to Minh, this is what he saw on most working pibs.
+                wr.write_uint32(0xFFFFE334) #--Retraso: add 7 groups at 0x84
+                wr.write_uint32(0xFFFFECCD)
+                wr.write_uint32(0xFFFFE334)
+                wr.write_uint32(0xFFFFECCD)
+                wr.write_uint64(0)
+                wr.write_uint32(0)
+            BinaryCombine(rd, wr, 0x114)
+            #--DDS Data--
+            DDSLength = 0
+            DDSActual = BinaryCombine(rd, wr, 0x4)
+            startOfDDS = rd.pos()
+            for search in range(99999):
+                if (rd.pos() == rd.size()): #Reaches end of file
+                    break
+                DDSCheck = rd.read_uint32()
+                if (DDSCheck == DDSActual): #Reaches another DDS node
+                    rd.seek(rd.pos() - 408)
+                    DDSBool = True
+                    break
+            endOfDDS = rd.pos()
+            rd.seek(startOfDDS)
+            DDSLength = (endOfDDS) - (startOfDDS)
+            BinaryCombine(rd, wr, DDSLength)
+    
+    #//Write//
+    with open(ishin_path, "wb") as f:
+        f.write(wr.buffer())
+    return ishin_path + " converted!"
+
 if (len(sys.argv) <= 1):
     print('Drag and drop a Yakuza Ishin .pib file OR folder containing .pib files for the Pib Converter to work.')
     input("Press ENTER to exit... ")
 else:
+    numOfFiles = 0
     propertyPath = sys.argv[1]
     if(propertyPath.endswith(".pib")):
-        #//Read//
-        wr = BinaryReader()
-        wr.set_endian(True)
-        with open(propertyPath, "rb") as f:
-            rd = BinaryReader(f.read())
-            rd.set_endian(True)
-
-            #--File Header--
-            fileInfo = BinaryCombine(rd, wr, 0x8)
-            fileVersion = rd.read_uint32()
-            if (fileVersion == 25):
-                wr.write_uint32(27)
-            fileFunny = BinaryCombine(rd, wr, 4)
-            #--File Information--
-            pibID = BinaryCombine(rd, wr, 4)
-            numOfDDS = BinaryCombine(rd, wr, 4)
-            BinaryCombine(rd, wr, 0x2A8)
-
-            for DDS in range(numOfDDS):
-                print("DDS #"+str(DDS+1)+" Done")
-                #--DDS Info--
-                BinaryCombine(rd, wr, 0x8)
-                if (fileVersion == 25): #--Retraso: add group at 0x4 
-                    wr.write_uint32(0)
-                BinaryCombine(rd, wr, 0x78)
-                if (fileVersion == 25): #Credit to Minh, this is what he saw on most working pibs.
-                    wr.write_uint32(0xFFFFE334) #--Retraso: add 7 groups at 0x84
-                    wr.write_uint32(0xFFFFECCD)
-                    wr.write_uint32(0xFFFFE334)
-                    wr.write_uint32(0xFFFFECCD)
-                    wr.write_uint64(0)
-                    wr.write_uint32(0)
-                BinaryCombine(rd, wr, 0x114)
-                #--DDS Data--
-                DDSLength = 0
-                DDSActual = BinaryCombine(rd, wr, 0x4)
-                startOfDDS = rd.pos()
-                for search in range(99999):
-                    if (rd.pos() == rd.size()):
-                        print("Check #1 | Reached End of File")
-                        break
-                    DDSCheck = rd.read_uint32()
-                    if (DDSCheck == DDSActual):
-                        rd.seek(rd.pos() - 408)
-                        DDSBool = True
-                        print("Check #2 | Reached New DDS File")
-                        break
-                endOfDDS = rd.pos()
-                rd.seek(startOfDDS)
-                DDSLength = (endOfDDS) - (startOfDDS)
-                BinaryCombine(rd, wr, DDSLength)
-        
-        with open(propertyPath + " new.pib", "wb") as f:
-            f.write(wr.buffer())
+        print(IshinWrite(propertyPath))
+    elif(os.path.isdir(propertyPath)):
+        for path, dirs, files in os.walk(propertyPath):
+            for f in files:
+                file_path = os.path.join(path, f)
+                if file_path.endswith(".pib"):
+                    print(IshinWrite(file_path))
+    else:
+        print("Bad filetype.")
